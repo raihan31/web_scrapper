7e7716550c31ae7639eb5d5487a5e6cc60384dae
raihan.rubel <abu.rubel@selise.ch>
2019-04-25 20:25:13 +0600

    configura

---
+++ db_config.py
@@ -0,0 +1,30 @@
+hostname = 'localhost'
+username = 'USERNAME'
+password = 'PASSWORD'
+database = 'DBNAME'
+
+# Simple routine to run a query on a database and print the results:
+def doQuery( conn ) :
+    cur = conn.cursor()
+    cur.execute( "SELECT fname, lname FROM employee" )
+    for firstname, lastname in cur.fetchall() :
+        print (firstname, lastname)
+print ("Using psycopg2…")
+import psycopg2
+myConnection = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
+doQuery( myConnection )
+myConnection.close()
+
+print ("Using PyGreSQL…")
+import pgdb
+myConnection = pgdb.connect( host=hostname, user=username, password=password, database=database )
+doQuery( myConnection )
+myConnection.close()
+
+class MedExDb:
+    hostname = 'localhost'
+    username = 'USERNAME'
+    password = 'PASSWORD'
+    database = 'DBNAME'
+    def __init__(self, host, user, password, db_name):
+        
